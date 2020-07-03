d3f74ba31eb383a39b37df8a20b28eccca295cd9
Rabee <rabeerd@gmail.com>
2020-07-03 17:34:30 +0200

    new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Deployed successfully</p>
+        <p>Deployed successfully ...!</p>
         <a
           className='App-link'
           href='https://reactjs.org'
