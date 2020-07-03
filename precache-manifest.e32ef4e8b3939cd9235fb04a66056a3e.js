643413e607c7fded169671dbd7b5e46602426cbe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-03 15:35:59 +0000

    Deploy rabrad/CI-CD-with-travis-Heroku to github.com/rabrad/CI-CD-with-travis-Heroku.git:gh-pages

---
+++ precache-manifest.e32ef4e8b3939cd9235fb04a66056a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a275f1374bd19179dfae5eb429491a",
+    "url": "/CI-CD-with-travis-Heroku/index.html"
+  },
+  {
+    "revision": "1ee39cc627b65a822bd6",
+    "url": "/CI-CD-with-travis-Heroku/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bbfcc43f9e31620da35",
+    "url": "/CI-CD-with-travis-Heroku/static/js/2.0761c17e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CI-CD-with-travis-Heroku/static/js/2.0761c17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee39cc627b65a822bd6",
+    "url": "/CI-CD-with-travis-Heroku/static/js/main.34479ae2.chunk.js"
+  },
+  {
+    "revision": "1a8849f8e9aa180a6e2d",
+    "url": "/CI-CD-with-travis-Heroku/static/js/runtime-main.4fc7979f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CI-CD-with-travis-Heroku/static/media/logo.5d5d9eef.svg"
+  }
+]);
